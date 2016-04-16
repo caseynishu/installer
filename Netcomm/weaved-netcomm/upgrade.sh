bdce7cf123f9d66f663d7b1f90577a31805c84a0
Gary Worsham <faultline1989@yahoo.com>
2016-04-15 17:15:21 -0700

    Added version 02 of Netcomm installer.

--- Netcomm/weaved-netcomm/upgrade.sh
+++ Netcomm/weaved-netcomm/upgrade.sh
@@ -1,11 +1,40 @@
 #!/bin/sh
-/etc/init.d/rc.d/weavedSSH stop
-/etc/init.d/rc.d/weavedSCH stop
-/etc/init.d/rc.d/weavedWEB stop
-/etc/init.d/rc.d/weavedRMT3 stop
+#
+# Weaved Image Updater for netcomm modems
+#
+
+
+#config
+WEAVED_DIR="/etc/weaved"
+PROVISION_DEFAULT="$WEAVED_DIR/pfiles"
+DEVICES_ACTIVE="$WEAVED_DIR/active"
+DEVICES_AVAILABLE="$WEAVED_DIR/available"
+INIT_DIR="/etc/init.d/rc.d"
+
+
+# shut all weaved services down
+for file in $INIT_DIR/weav*
+do                    
+    eval "$file stop"
+done
+
+
+# grab latest netcomm weaved install
+
 wget https://github.com/weaved/installer/raw/master/Netcomm/netcomm.tgz -O /root/netcomm.tgz --no-check-certificate
 cd /root
 tar xvzf netcomm.tgz
 cd weaved-netcomm
 chmod +x install
 ./install
+
+
+
+# start every thing back up
+for file in $INIT_DIR/weav*                         
+do                                                  
+    eval "$file start"                            
+done  
+
+
+
