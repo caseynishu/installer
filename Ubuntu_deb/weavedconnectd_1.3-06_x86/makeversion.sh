276ef19c246cf4a2e55389a878d2985afb406271
Gary Worsham <faultline1989@yahoo.com>
2016-06-30 19:46:51 -0700

    Add utility scripts.  Update remot3_script_lib

---
+++ Ubuntu_deb/weavedconnectd_1.3-06_x86/makeversion.sh
@@ -0,0 +1,1 @@
+mv weavedconnectd_1.3-06_x86.deb weavedconnectd_1.3-06$1_x86.deb
---
+++ Ubuntu_deb/weavedconnectd_1.3-06_x86/sgwi.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# sgwi.sh
+# allows quick editing of the Weaved installer script on Ubuntu
+# system and removing backup file to avoid warning/error
+BINDIR=/usr/bin
+packageName=weavedconnectd_1.3-06_x86
+
+sudo gedit "$packageName"$BINDIR/weavedinstaller "$packageName"$BINDIR/weavedinstaller_OEM "$packageName"$BINDIR/weavedinstallerlib "$packageName"$BINDIR/weavedfactoryreset
+sudo rm "$packageName"$BINDIR/*~
+
