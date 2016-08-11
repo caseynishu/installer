a66298e2fd8457c0e49660594e2ec1681902652b
Gary Worsham <faultline1989@yahoo.com>
2016-08-11 08:06:45 -0700

    Fixed lintpkg.sh to make armel version.

--- Raspbian deb/1.3-07/lintpkg.sh
+++ Raspbian deb/1.3-07/lintpkg.sh
@@ -3,34 +3,30 @@
 # script to build Debian package for Weaved Installer
 # sorts out Lintian errors/warnings into individual
 # text files
-
 pkg=weavedconnectd
 pkgFolder=weavedconnectd_1.3-07
+
+gzip -9 "$pkgFolder"/usr/share/doc/$pkg/*.man
+sudo chown root:root "$pkgFolder"/usr/share/doc/$pkg/*.gz
 
 echo "Menu"
 echo "1) armhf"
 echo "2) armel"
 read archMenu
 
+# set architecture
+controlFile="$pkgFolder"/DEBIAN/control
+
 if [ $archMenu -eq 1 ]; then
     arch="_armhf"
+     sed 's/Architecture: arm.*/Architecture: armhf/' < "$controlFile" > /tmp/control
 elif [ $archMenu -eq 2 ]; then
     arch="_armel"
+    sed 's/Architecture: arm.*/Architecture: armel/' < "$controlFile" > /tmp/control
 fi
 
-gzip -9 "$pkgFolder"/usr/share/doc/$pkg/*.man
-sudo chown root:root "$pkgFolder"/usr/share/doc/$pkg/*.gz
-
-# set architecture
-if [ $archMenu -eq 1 ]; then
-     sed 's/"Architecture: arm.*"/"Architecture: armhf"/' < "$pkgFolder"/DEBIAN/control > /tmp/control
-elif [ $archMenu -eq 2 ]; then
-     sed 's/"Architecture: arm.*"/"Architecture: armel"/' < "$pkgFolder"/DEBIAN/control > /tmp/control
-fi
-
-controlFile="$pkgFolder"/DEBIAN/control
 sudo chmod a+w "$controlFile"
-sudo mv /tmp/control "$controlFile"
+sudo cp /tmp/control "$controlFile"
 sudo chmod 644 "$controlFile"
 
 # now build the deb file, then rename it to add architecture
@@ -38,10 +34,10 @@ dpkg-deb --build "$pkgFolder"
 
 version=$(grep -i version "$controlFile" | awk '{ print $2 }')
 
-mv "$pkgFolder".deb "$pkg$version$arch".deb
+mv "$pkgFolder".deb "$pkg"_"$version$arch".deb
 
 # scan result for errors and warnings
-lintian -EviIL +pedantic "$pkg$version$arch".deb  > lintian-result.txt
+lintian -EviIL +pedantic "$pkg"_"$version$arch".deb  > lintian-result.txt
 grep E: lintian-result.txt > lintian-E.txt
 grep W: lintian-result.txt > lintian-W.txt
 grep I: lintian-result.txt > lintian-I.txt
