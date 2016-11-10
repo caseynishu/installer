3eae9eda67aa7f6edac7d7794f1c054442d7adbf
Gary Worsham <faultline1989@yahoo.com>
2016-11-10 11:52:58 -0800

    Added logging to maintainer scripts. Fixed weavedstop.sh to not display anything if no enablements are installed.

--- Raspbian deb/1.3-07/weavedconnectd_1.3-07/usr/bin/weavedstop.sh
+++ Raspbian deb/1.3-07/weavedconnectd_1.3-07/usr/bin/weavedstop.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-# Updated October 31, 2016
+# Updated November 10, 2016
 
 # weavedstop.sh, stops all running Weaved services
 
-if [ -e /usr/bin/Weaved*.sh ]; then
+if [[ $EUID -ne 0 ]]; then
+    echo "Running $0 requires root access." 1>&2
+    echo "Please run sudo $0 instead of $0." 1>&2
+    exit 1
+fi
+
+
+if [ -d "/usr/bin" ]; then
     for f in /usr/bin/Weaved*.sh; do
-	$f stop
+        if [ -f $f ]; then
+	    $f stop
+	fi
     done
 fi
 
