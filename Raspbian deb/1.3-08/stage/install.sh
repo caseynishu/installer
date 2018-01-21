68f2763a5bbd5212c37814c5752a233eae85ad6d
root <faultline1989@yahoo.com>
2018-01-21 14:43:38 -0800

    Updates for OEM ARM package and self-extracting archive.

--- Raspbian deb/1.3-08/stage/install.sh
+++ Raspbian deb/1.3-08/stage/install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-tar xvzf connectd_1.3-08h_armhf_BETA.gz
-rm connectd_1.3-08h_armhf_BETA.gz
+FILE=connectd_1.3-08h_armhf_BETA
+tar xvzf $FILE.gz
+rm $FILE.gz
 PWD=$(pwd)
-sed -i '/^BASEDIR=/c\BASEDIR="$PWD" usr/bin/connectd_options
-sed -i '/^BASEDIR=/c\BASEDIR="$PWD" usr/bin/connectd_installer_deprecated
-sed -i '/^BASEDIR=/c\BASEDIR="$PWD" usr/bin/connectd_register_deprecated
-sed -i '/^BASEDIR=/c\BASEDIR="$PWD" etc/init.d/connectd
-sed -i '/^BASEDIR=/c\BASEDIR="$PWD" etc/init.d/connectd_schannel
+sed -i "/^BASEDIR=/c\BASEDIR=$PWD" usr/bin/connectd_options
+sed -i "/^BASEDIR=/c\BASEDIR=$PWD" usr/bin/connectd_control
+sed -i "/^BASEDIR=/c\BASEDIR=$PWD" usr/bin/connectd_start
+sed -i "/^BASEDIR=/c\BASEDIR=$PWD" usr/bin/connectd_installer_deprecated
+sed -i "/^BASEDIR=/c\BASEDIR=$PWD" usr/bin/connectd_register_deprecated
+sed -i "/^BASEDIR=/c\BASEDIR=$PWD" etc/init.d/connectd
+sed -i "/^BASEDIR=/c\BASEDIR=$PWD" etc/init.d/connectd_schannel
